2a01efa8fd87ed2eb27b181e6a16e093e83b82f5
klokov <dnwjdgus1994@gmail.com>
2021-06-10 22:09:33 +0900

    Preparation for Migrating WorkoutSet.kt with test

---
+++ android/app/src/androidTest/java/kr/valor/bal/data/MigrationTest.kt
@@ -0,0 +1,110 @@
+package kr.valor.bal.data
+
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
+import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.room.testing.MigrationTestHelper
+import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import androidx.test.platform.app.InstrumentationRegistry
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import kr.valor.bal.data.entities.WorkoutSet
+import kr.valor.bal.utilities.DATABASE_NAME
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class MigrationTest {
+
+    private val TEST_DB_NAME = "migration-test"
+
+    private val WEIGHTS_COLUMN_INDEX = 3
+
+    private lateinit var db: SupportSQLiteDatabase
+
+    @get:Rule
+    val helper: MigrationTestHelper = MigrationTestHelper(
+        InstrumentationRegistry.getInstrumentation(),
+        AppDatabase::class.java.canonicalName,
+        FrameworkSQLiteOpenHelperFactory()
+    )
+
+    @Before
+    fun setUp() {
+        db = helper.createDatabase(TEST_DB_NAME, 1)
+    }
+
+    @After
+    fun closeDb() {
+        helper.closeWhenFinished(db)
+    }
+
+
+    @Test
+    fun beforeMigrating_containsCorrectData() {
+        val values = ContentValues()
+        values.put("container_id", 10L)
+        values.put("reps", 10)
+        values.put("weights", 100)
+
+        val id = db.insert("workout_set", SQLiteDatabase.CONFLICT_REPLACE, values)
+
+        val cursor = db.query("SELECT * FROM workout_set WHERE set_id = ?", arrayOf(id))
+
+        assertThat(cursor, `is`(notNullValue()))
+        assertThat(cursor.moveToFirst(), `is`(true))
+
+        val weightsColumnName = cursor.getColumnName(WEIGHTS_COLUMN_INDEX)
+        assertThat(weightsColumnName, `is`("weights"))
+
+        val weightsColumnType = cursor.getType(WEIGHTS_COLUMN_INDEX)
+        assertThat(weightsColumnType, `is`(Cursor.FIELD_TYPE_INTEGER))
+    }
+
+    // How to properly add indices in database migration for Room?
+    // https://stackoverflow.com/questions/54269102/how-to-properly-add-indices-in-database-migration-for-room
+    @Test
+    fun migrate1To2_containsCorrectData() {
+
+        val values = ContentValues()
+        values.put("container_id", 10L)
+        values.put("reps", 10)
+        values.put("weights", 100)
+
+        val id = db.insert("workout_set", SQLiteDatabase.CONFLICT_REPLACE, values)
+
+        db.close()
+
+        db = helper.runMigrationsAndValidate(TEST_DB_NAME, 2, true, MIGRATION_1_2)
+
+        val cursor = db.query("SELECT * FROM workout_set WHERE set_id = ?", arrayOf(id))
+
+        assertThat(cursor, `is`(notNullValue()))
+        assertThat(cursor.moveToFirst(), `is`(true))
+
+        val weightsColumnName = cursor.getColumnName(WEIGHTS_COLUMN_INDEX)
+        assertThat(weightsColumnName, `is`("weights"))
+
+        val weightsColumnType = cursor.getType(WEIGHTS_COLUMN_INDEX)
+        assertThat(weightsColumnType, `is`(Cursor.FIELD_TYPE_FLOAT))
+
+    }
+
+}
--- android/app/src/main/java/kr/valor/bal/data/AppDatabase.kt
+++ android/app/src/main/java/kr/valor/bal/data/AppDatabase.kt
@@ -10,7 +10,7 @@ import kr.valor.bal.data.entities.WorkoutOverview
 import kr.valor.bal.data.entities.WorkoutSet
 import javax.inject.Singleton
 
-@Database(entities = [WorkoutOverview::class, WorkoutDetail::class, WorkoutSet::class], version = 2, exportSchema = false)
+@Database(entities = [WorkoutOverview::class, WorkoutDetail::class, WorkoutSet::class], version = 2, exportSchema = true)
 @TypeConverters(Converters::class)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun workoutDao(): WorkoutDao
@@ -18,6 +18,24 @@ abstract class AppDatabase: RoomDatabase() {
 
 val MIGRATION_1_2 = object : Migration(1,2) {
     override fun migrate(database: SupportSQLiteDatabase) {
-        // TODO
+        database.execSQL(
+            "CREATE TABLE workout_set_new (" +
+                    "set_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                    "container_id INTEGER NOT NULL, " +
+                    "reps INTEGER NOT NULL, " +
+                    "weights REAL NOT NULL, " +
+                    "FOREIGN KEY(container_id) REFERENCES " +
+                    "workout_detail(detail_id) ON UPDATE NO ACTION ON DELETE CASCADE)"
+        )
+
+        database.execSQL(
+            "INSERT INTO workout_set_new (set_id, container_id, reps, weights) SELECT * FROM workout_set"
+        )
+
+        database.execSQL("DROP TABLE workout_set")
+
+        database.execSQL("ALTER TABLE workout_set_new RENAME TO workout_set")
+
+        database.execSQL("CREATE INDEX index_workout_set_container_id ON workout_set (container_id)")
     }
 }
